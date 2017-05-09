9f7d8985c85eb3c0ab99a6fe3c89eeaffb0da4e0
S.Om <ruddy.rudeman@gmail.com>
2017-05-10 01:22:38 +0300

    - Changed GUI to Qt Designer ui template - Changed color scheme - Added port selector

--- plottipy.py
+++ plottipy.py
@@ -3,126 +3,136 @@
 import pyqtgraph as pg
 from pyqtgraph.dockarea import DockArea, Dock, DockDrop
 import time
+import os
 from pyqtgraph.Qt import QtCore, QtGui, QtWidgets
 import numpy as np
 import serial
 from serial.tools import list_ports
 
+pg.setConfigOption('background', pg.mkColor('#31363b'))
+
 # Create main window
-application = QtGui.QApplication([])
-mainWindow = QtGui.QMainWindow()
-mainWindow.resize(1000,500)
-mainWindow.setWindowTitle('Serial plotter')
+pg.mkQApp()
 
-# Create dock layout area
-area = DockArea()
-mainWindow.setCentralWidget(area)
+# Define main window class from ui template
+WindowTemplate, TemplateBaseClass = pg.Qt.loadUiType('plottipy.ui')
 
-# Create docks
-display_dock = Dock("Display", size=(1000,400))
-display_dock.hideTitleBar()
+class MainWindow(TemplateBaseClass):
+    def __init__(self):
+        TemplateBaseClass.__init__(self)
 
-port_dock = Dock("Port", size=(1000,100))
-port_list = QtWidgets.QComboBox()
-port_list.addItem("Random generator")
-for port in list_ports.comports():
-    port_list.addItem(f"{port[0]} - {port[1]}")
-port_dock.addWidget(port_list, col=0)
+        # Create the main window
+        self.ui = WindowTemplate()
+        self.ui.setupUi(self)
 
-settings_dock = Dock("Settings")     ## give this dock the minimum possible size
+        self.ui.plot.getPlotItem().setLabel('right', '')
+        self.ui.plot.getPlotItem().setLabel('top', '')
+        self.ui.plot.getPlotItem().setLabel('left', '')
+        self.ui.plot.getPlotItem().setLabel('bottom', '')
+        self.ui.plot.setDownsampling(mode='peak')
+        self.ui.plot.setClipToView(True)
+        self.ui.plot.setRange(xRange=[-100, 0])
+        self.ui.plot.setLimits(xMax=0)
 
-# Lay docks in area
-area.addDock(display_dock, 'top')
-area.addDock(settings_dock, 'bottom')
-area.addDock(port_dock, 'above', settings_dock)
+        self.portSelector = PortSelector(self.ui.portList, self.ui.port_open_b, self.ui.port_close_b, self.ui.port_refresh_b)
 
-# Fill docks
-cw = QtGui.QWidget()
+        self.curve = self.ui.plot.plot()
+        self.data = np.empty(100)
+        self.ptr = 0
 
-l = QtGui.QVBoxLayout()
-cw.setLayout(l)
+    def update(self, data):
+        self.data[self.ptr] = data
+        self.ptr += 1
+        if self.ptr >= self.data.shape[0]:
+            tmp = self.data
+            self.data = np.empty(self.data.shape[0] * 2)
+            self.data[:tmp.shape[0]] = tmp
+        self.curve.setData(self.data[:self.ptr])
+        self.ui.plot.getPlotItem().setLabel('top', "%f" % self.ptr)
+        self.curve.setPos(-self.ptr, 0)
 
-w = pg.QtGui.QPlainTextEdit()
+class PortSelector():
+    def __init__(self,
+                 list_w:QtWidgets.QListWidget,
+                 open_b:QtWidgets.QPushButton,
+                 close_b:QtWidgets.QPushButton,
+                 refresh_b:QtWidgets.QPushButton):
+        self.list_w = list_w
+        self.open_b = open_b
+        self.close_b = close_b
+        self.refresh_b = refresh_b
 
-settings_dock.addWidget(w)
+        self.ports = []
 
-p3 = pg.PlotWidget(name='Plot1')  ## giving the plots names allows us to link their axes together
-p3.getPlotItem().setLabel('right','')
-p3.getPlotItem().setLabel('top','')
-p3.getPlotItem().setLabel('left','')
-p3.getPlotItem().setLabel('bottom','')
-l.addWidget(p3)
+        self.refresh_b.clicked.connect(self.refresh)
 
-display_dock.addWidget(cw)
+        self.refresh()
+
+    def refresh(self):
+        self.ports = [('RND', 'Random generator'),]
+        self.ports += list(list_ports.comports())
+        self.list_w.clear()
+        for port in self.ports:
+            self.list_w.addItem(f"{port[0]} - {port[1]}")
+
+    def open(self):
+        pass
+
+    def close(self):
+        pass
 
 
 
-p3.setDownsampling(mode='peak')
-p3.setClipToView(True)
+win = MainWindow()
 
-p3.setRange(xRange=[-100,0])
-p3.setLimits(xMax=0)
-# p3.disableAutoRange()
-# p3.setAutoPan(x=True)
-curve3 = p3.plot()
-# curve4 = p3.plot()
+# vLine = pg.InfiniteLine(angle=90, movable=False)
+# hLine = pg.InfiniteLine(angle=0, movable=False)
+# p3.addItem(vLine, ignoreBounds=True)
+# p3.addItem(hLine, ignoreBounds=True)
 
-data3 = np.empty(100)
-ptr3 = 0
+# def mouseMoved(evt):
+#     pos = p3.lastMousePos
+#     if p3.sceneBoundingRect().contains(pos):
+#         mousePoint = p3.getPlotItem().vb.mapSceneToView(pos)
+#         index = int(mousePoint.x())
+#
+#         vLine.setPos(mousePoint.x())
+#         hLine.setPos(mousePoint.y())
+#
+# p3.scene().sigMouseHover.connect(mouseMoved)
+#
+# def update():
+#     global data3, ptr3
+#     data3[ptr3] = np.random.normal()
+#     ptr3 += 1
+#     if ptr3 >= data3.shape[0]:
+#         tmp = data3
+#         data3 = np.empty(data3.shape[0] * 2)
+#         data3[:tmp.shape[0]] = tmp
+#     curve3.setData(data3[:ptr3])
+#     p3.getPlotItem().setLabel('top', "%f" % ptr3)
+#     curve3.setPos(-ptr3, 0)
+#     # curve4.setData(data3[:ptr3])
+#     # curve4.setPos(-ptr3, 0)
 
-vLine = pg.InfiniteLine(angle=90, movable=False)
-hLine = pg.InfiniteLine(angle=0, movable=False)
-p3.addItem(vLine, ignoreBounds=True)
-p3.addItem(hLine, ignoreBounds=True)
-
-def mouseMoved(evt):
-    pos = p3.lastMousePos
-    if p3.sceneBoundingRect().contains(pos):
-        mousePoint = p3.getPlotItem().vb.mapSceneToView(pos)
-        index = int(mousePoint.x())
-
-        vLine.setPos(mousePoint.x())
-        hLine.setPos(mousePoint.y())
-
-p3.scene().sigMouseHover.connect(mouseMoved)
-
-def update():
-    global data3, ptr3
-    data3[ptr3] = np.random.normal()
-    ptr3 += 1
-    if ptr3 >= data3.shape[0]:
-        tmp = data3
-        data3 = np.empty(data3.shape[0] * 2)
-        data3[:tmp.shape[0]] = tmp
-    curve3.setData(data3[:ptr3])
-    p3.getPlotItem().setLabel('top', "%f" % ptr3)
-    curve3.setPos(-ptr3, 0)
-    # curve4.setData(data3[:ptr3])
-    # curve4.setPos(-ptr3, 0)
-
-class Gen(QtCore.QThread):
+class Generator(QtCore.QThread):
     ''' Represents a punching bag; when you punch it, it
         emits a signal that indicates that it was punched. '''
-    punched = QtCore.Signal()
+    newData = QtCore.Signal(float)
 
     def __init__(self, f):
         QtCore.QThread.__init__(self)
-        self.punched.connect(f)
+        self.newData.connect(f)
 
     def run(self):
         while 1:
             time.sleep(0.01)
-            self.punched.emit()
+            self.newData.emit(np.random.normal())
 
-
-# timer = pg.QtCore.QTimer()
-# timer.timeout.connect(update)
-# timer.start(5)
-
-gen = Gen(update)
+gen = Generator(win.update)
 gen.start()
 
-mainWindow.show()
+win.show()
 
 ## Start Qt event loop unless running in interactive mode or using pyside.
 if __name__ == '__main__':
